a138c0f6eb85f5d01b481f0e630e8e0502b69c77
Ed Zynda <ezynda3@gmail.com>
2016-05-22 16:07:46 -0700

    Create model for tasks and GetTask func

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
+	"github.com/ezynda3/gotodo/models"
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+
+	db := initDB("storage.db")
 
 	r := gin.Default()
 
@@ -14,9 +19,22 @@ func main() {
 		c.String(http.StatusOK, "pong")
 	})
 
-	r.GET("/tasks", func(c *gin.Context) {})
+	r.GET("/tasks", func(c *gin.Context) {
+		c.JSON(http.StatusOK, models.GetTasks(db))
+	})
 	r.POST("/tasks", func(c *gin.Context) {})
 	r.DELETE("/tasks/:id", func(c *gin.Context) {})
 
 	r.Run("localhost:8000")
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		panic(err)
+	}
+	if db == nil {
+		panic("db nil")
+	}
+	return db
 }
